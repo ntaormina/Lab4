9a72d743349c975a20308bfbe4a86dfb89dd92bc
Nik Taormina <c15nikolas.taormina@usafa.edu>
2013-10-24 17:20:02 -0600

    B Functionality

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <msp430.h> 
 #include "lcd.h"
+#include "buttons/button.h"
 
 /*
  * main.c
@@ -11,6 +12,11 @@ void main(void) {
 
     char stringTop[] = "ECE382 is my favorite class!";
     char stringBottom[] = "ECE321 is not my favorite class!";
+    char message[] = "Message?";
+    char press[] = "Press123";
+    char button1[] = "You're number 1!";
+    char button2[] = "You're number 2!";
+    char button3[] = "You're number 3!";
 
     initSPI();
 
@@ -18,6 +24,85 @@ void main(void) {
 
     LCDclear();
 
+    cursorToLineOne();
+
+    writeString(message);
+
+    cursorToLineTwo();
+
+    writeString(press);
+
+
+    configureP1PinAsButton(BIT1|BIT2|BIT3);         // configure pins 1, 2, and 3 as buttons
+
+        P1DIR |= BIT0|BIT6;                             // set launchpad LEDs to output
+
+        while (1) {
+            char buttons[] = {BIT1, BIT2, BIT3};
+            char pressedButton = pollP1Buttons(buttons, 3);
+
+            switch (pressedButton) {
+                case BIT1:
+                	 while(1){
+
+
+
+                	    cursorToLineOne();
+
+                	    writeString(stringTop);
+
+                	    scrollString(stringTop, getStringLength(stringTop));
+
+                	    cursorToLineTwo();
+
+                	    writeString(button1);
+
+                	    scrollString(button1, getStringLength(button1));
+
+                	    }
+                    break;
+                case BIT2:
+                	 while(1){
+
+
+
+                	    cursorToLineOne();
+
+                	    writeString(stringTop);
+
+                	    scrollString(stringTop, getStringLength(stringTop));
+
+                	    cursorToLineTwo();
+
+                	    writeString(button2);
+
+                	    scrollString(button2, getStringLength(button2));
+
+                	    }
+                    break;
+                case BIT3:
+                	 while(1){
+
+
+
+                	    cursorToLineOne();
+
+                	    writeString(stringTop);
+
+                	    scrollString(stringTop, getStringLength(stringTop));
+
+                	    cursorToLineTwo();
+
+                	    writeString(button3);
+
+                	    scrollString(button3, getStringLength(button3));
+
+                	    }
+                    P1OUT ^= BIT0|BIT6;
+                    waitForP1ButtonRelease(BIT3);
+                    break;
+            }
+        }
 
 
     while(1){
